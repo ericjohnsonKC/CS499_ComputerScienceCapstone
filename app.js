0a278b654b27c48c6a6083f83e55556d51dd02f2
Eric Johnson <ericjohnson.kc@gmail.com>
2021-05-31 15:31:25 -0500

    Made a change and then undid the change to styles.css.  The real edit that was made that this commit is for was an edit to app.js.  The change was in the search bar functionality.  Prior to the change, there was a problem with case sensitivity of searches.  The edit I made resolved the problem.

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function(){
       const books = list.getElementsByTagName('li');
       Array.from(books).forEach((book) => {
         const title = book.firstElementChild.textContent;
-        if(title.toLowerCase().indexOf(e.target.value) != -1){
+        if(title.toLowerCase().indexOf(term) != -1){
           book.style.display = 'block';
         } else {
           book.style.display = 'none';
